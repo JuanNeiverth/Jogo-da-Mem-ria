d5d5f83569ef82e7d08b38fb15631e699c02ba37
Juan <polli.neiverth@gmail.com>
2023-02-11 11:45:55 -0600

    Initial Commit

---
+++ Game to memorizee/Game_to_memorizeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_to_memorizeeApp.swift
+//  Game to memorizee
+//
+//  Created by Juan on 11/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Game_to_memorizeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
