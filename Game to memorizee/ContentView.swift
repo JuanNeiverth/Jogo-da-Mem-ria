9ca22c200e8adae102441e49952df0990aa3ab58
JuanNeiverth <polli.neiverth@gmail.com>
2023-02-13 20:47:05 -0600

    Falta ainda terminar

--- Game to memorizee/ContentView.swift
+++ Game to memorizee/ContentView.swift
@@ -9,15 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            ForEach(0..<4){ index in
+           CardView()
+        }
         }
         .padding()
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
+        
+        
+        
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool = true
+    var body: some View {
+        ZStack{
+            if isFaceUp {
+                RoundedRectangle(cornerRadius:10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius:10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius:10.0).fill()
+            }
+      }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
